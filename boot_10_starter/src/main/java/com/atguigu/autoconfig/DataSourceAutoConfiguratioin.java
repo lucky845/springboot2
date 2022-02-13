8b7fb3dc1e3246d6a3c6b05ef6167dfc866325fe
lucky845 <lc1447545564@gmail.com>
2022-02-13 14:47:21 +0800

    恢复了丢失的模块

--- boot_10_starter/src/main/java/com/atguigu/autoconfig/DataSourceAutoConfiguratioin.java
+++ boot_10_starter/src/main/java/com/atguigu/autoconfig/DataSourceAutoConfiguratioin.java
@@ -2,26 +2,39 @@ package com.atguigu.autoconfig;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.atguigu.properties.DataSourceProperties;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 
-@SpringBootConfiguration
+@Configuration
 @EnableConfigurationProperties(DataSourceProperties.class)
-public class DataSourceAutoConfiguration {
+public class DataSourceAutoConfiguratioin {
 
     @Autowired
     private DataSourceProperties dataSourceProperties ;
 
     @Bean
+    @ConditionalOnProperty(value = "spring.jdbc.datasource.type",havingValue = "druid")
     public DataSource createDataSource(){
         DruidDataSource dataSource = new DruidDataSource();
         dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
         dataSource.setUrl(dataSourceProperties.getUrl());
         dataSource.setUsername(dataSourceProperties.getUsername());
+        dataSource.setPassword(dataSourceProperties.getPassword());
+        return dataSource;
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "spring.jdbc.datasource.type",havingValue = "c3p0")
+    public DataSource createC3P0DataSource() throws Exception{
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(dataSourceProperties.getDriverClassName());
+        dataSource.setJdbcUrl(dataSourceProperties.getUrl());
+        dataSource.setUser(dataSourceProperties.getUsername());
         dataSource.setPassword(dataSourceProperties.getPassword());
         return dataSource;
     }
